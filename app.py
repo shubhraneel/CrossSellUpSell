f736cb6a1062d0439aec4306c004df316d397600
shubhraneel <shubhraneelpal@gmail.com>
2021-05-02 11:54:51 +0530

    F

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/recommendations/<wholesaler_id>')
+def recommendations(wholesaler_id):
+  return wholesaler_id
+
 if __name__ == '__main__':
   app.run(debug=True)
