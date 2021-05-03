b5c8f76eee3bb6dfbc4fd05d753f4db4c0c798fa
shubhraneel <shubhraneelpal@gmail.com>
2021-05-03 08:32:42 +0530

    Use material recommendation model to predict

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+from material_recommender import predict
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  materials = json.load(open('data/wudi.json'))
+  return render_template('index.html', materials=materials)
+
+@app.route('/recommendations', methods=['POST'])
+def recommendations():
+  return redirect(f"/recommendations/{request.form['wholesaler']}")
 
 @app.route('/recommendations/<wholesaler_id>')
-def recommendations(wholesaler_id):
-  return wholesaler_id
+def recommendations_wholesaler(wholesaler_id):
+  return str(predict(int(wholesaler_id)))
 
 if __name__ == '__main__':
   app.run(debug=True)
---
+++ material_recommender.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import numpy as np
+from zipfile import ZipFile
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from pathlib import Path
+import operator
+import json
+
+# constants
+EMBEDDING_SIZE = 50
+
+# model
+class RecommenderNet(keras.Model):
+  def __init__(self, num_users, num_movies, embedding_size, **kwargs):
+    super(RecommenderNet, self).__init__(**kwargs)
+    self.num_users = num_users
+    self.num_movies = num_movies
+    self.embedding_size = embedding_size
+    self.user_embedding = layers.Embedding(
+        num_users,
+        embedding_size,
+        embeddings_initializer="he_normal",
+        embeddings_regularizer=keras.regularizers.l2(1e-6),
+    )
+    self.user_bias = layers.Embedding(num_users, 1)
+    self.movie_embedding = layers.Embedding(
+        num_movies,
+        embedding_size,
+        embeddings_initializer="he_normal",
+        embeddings_regularizer=keras.regularizers.l2(1e-6),
+    )
+    self.movie_bias = layers.Embedding(num_movies, 1)
+
+  def call(self, inputs):
+    user_vector = self.user_embedding(inputs[:, 0])
+    user_bias = self.user_bias(inputs[:, 0])
+    movie_vector = self.movie_embedding(inputs[:, 1])
+    movie_bias = self.movie_bias(inputs[:, 1])
+    dot_user_movie = tf.tensordot(user_vector, movie_vector, 2)
+    # Add all the components (including bias)
+    x = dot_user_movie + user_bias + movie_bias
+    # The sigmoid activation forces the rating to between 0 and 1
+    return tf.nn.sigmoid(x)
+
+# initial execution
+
+# reading id dictionaries
+f = open('data/wudi.json',)
+wudi = json.load(f)
+wudi = {int(k): v for k, v in wudi.items()}
+f.close()
+
+f = open('data/mudi.json',)
+mudi = json.load(f)
+mudi = {int(k): v for k, v in mudi.items()}
+f.close()
+
+num_wholesalers = len(np.unique(np.array(list(wudi.keys()))))
+num_materials = len(np.unique(np.array(list(mudi.keys()))))
+
+# loading pretrained model weights for prediction
+M = RecommenderNet(num_wholesalers, num_materials, EMBEDDING_SIZE)
+M.load_weights("models/material_recommed")
+
+def predict(wholesaler_id):
+
+  one_user_wholesaler=[]
+  wholesaler_id_transformed = wudi[wholesaler_id]
+  for i in range(num_materials):
+      one_user_wholesaler.append(wholesaler_id_transformed)
+  one_user_material=[]
+  for i in range(num_materials):
+      one_user_material.append(i)
+
+  one_user = pd.DataFrame({'wholesaler':one_user_wholesaler})
+  one_user['material'] = one_user_material
+  one_user["wholesaler"] = one_user_wholesaler
+
+  key_list_1 = list(mudi.keys())
+  val_list_1 = list(mudi.values())
+
+  preds=[]
+  for i in range(len(one_user)):
+      preds.append([float(M.predict(np.asarray([one_user.iloc[i]]))[0]), key_list_1[val_list_1.index(one_user['material'][i])]]) 
+  preds = np.array(preds)
+  indices = np.argsort(preds[:, 0])
+
+  return preds[indices[-5:]][:, 1]
