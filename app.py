2dd8df713fe0bd3bd331287c2923641a1fd153c2
shubhraneel <shubhraneelpal@gmail.com>
2021-05-08 12:14:15 +0530

    Add cart

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  materials = json.load(open('data/wudi.json'))
-  return render_template('index.html', materials=materials)
+  wholesalers = json.load(open('data/wudi.json'))
+  return render_template('index.html', wholesalers=wholesalers)
 
 @app.route('/recommendations', methods=['POST'])
 def recommendations():
@@ -18,11 +18,17 @@ def recommendations():
 def recommendations_wholesaler(wholesaler_id):
   materials = predict_material(int(wholesaler_id))
   quantities = [round(predict_HL(int(wholesaler_id), int(material_id)), 2) for material_id in materials]
+  all_materials = json.load(open('data/mudi.json'))
   return render_template(
     'recommendations.html', 
     wholesaler=wholesaler_id, 
-    materials_quantities=zip(materials, quantities)
+    materials_quantities=zip(materials, quantities),
+    all_materials=all_materials
     )
+
+@app.route('/order', methods=['POST'])
+def order_materials():
+  print(json.loads(request.data))
 
 if __name__ == '__main__':
   app.run(debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,47 @@
+let cart = {}
+
+function create_new_element(material, quantity) {
+  let element = document.createElement("div");
+  element.classList.add("order-list-element");
+  material_text = document.createTextNode(material);
+  quantity_text = document.createTextNode(quantity + " hL");
+  material_div = document.createElement("div");
+  material_div.classList.add("order-list-material");
+  material_div.appendChild(material_text);
+  quantity_div = document.createElement("div");
+  quantity_div.classList.add("order-list-quantity");
+  quantity_div.appendChild(quantity_text);
+  element.appendChild(material_div);
+  element.appendChild(quantity_div);
+  return element;
+}
+
+function add_to_cart(material, quantity) {
+  let parent = document.getElementById("orders");
+  parent.appendChild(create_new_element(material, quantity));
+  cart[material] = quantity;
+  console.log(cart)
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  if (event.target.elements.material.value == "" || event.target.elements.quantity.value == "")
+    return;
+  add_to_cart(event.target.elements.material.value, event.target.elements.quantity.value);
+}
+
+function handleOrder(wholesaler) {
+  data = {
+    "wholesaler": wholesaler,
+    "cart": cart,
+  }
+  fetch("/order", {
+    method: "POST", 
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  }).then(res => {
+    console.log("Request complete! response:", res);
+  });  
+}
